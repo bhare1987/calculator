a1535011d6d513061cc3f3d04f6881a2f488672c
bhare1987 <brandonlhare@gmail.com>
2016-02-16 15:31:19 -0500

    calculator update

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 function addListeners() {
- var buttons =  [].slice.call(document.querySelectorAll(".calButton"));
+ var buttons =  [].slice.call(document.querySelectorAll(".calButton")),
+ clear = document.getElementById('clear'),
+ evaluate = document.getElementById('evaluateCalc');
  buttons.forEach(function(item){
    item.addEventListener("click", getVal);
+   item.addEventListener("click", displayVal);
  });
+ clear.addEventListener("click", clearCalc);
+ evaluate.addEventListener("click", calculate);
 };
 
 function getVal(e) {
-  console.log(this.textContent);
    return this.textContent;
 };
+
+function displayVal() {
+  var display = document.getElementById('input'),
+  value = this.textContent;
+  if (display.innerHTML === "") {
+    display.textContent += value;
+  } else {
+  display.textContent += " " + value;
+  }
+}
+
+function clearCalc() {
+  var display = document.getElementById('input');
+  display.innerHTML = '';
+}
+
+function calculate() {
+  var calculation = document.getElementById('input').textContent.split(" ");
+  console.log(calculation);
+}
 
 addListeners();
